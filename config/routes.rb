e838f03ca687c22abd3477c0021ff57a4679b230
Dave Geffken <dave@cloudspace.com>
2014-02-27 14:57:37 -0500

    Added authed_request in home_controller and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ NewDay0::Application.routes.draw do
     post 'ajax_create' => 'devise/ajax_registrations#create', as: 'ajax_create'
 
   end
+
+  get 'authed_request' => 'home#authed_request', as: 'authed_request'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
